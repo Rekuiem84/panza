5ea5382e7ae7ed855629ba4648eb897b042140e1
Rekuiem84 <morinijoshua@gmail.com>
2024-03-12 14:40:19 +0100

    changements

--- index.php
+++ index.php
@@ -3,10 +3,13 @@
 
 require "classes/db.php";
 require "classes/log.php";
+
+session_start();
+
 $f = new log;
-$params=[];
-$errors=[];
-$message="";
+$params = [];
+$errors = [];
+$message = "";
 
 ?>
 <!DOCTYPE html>
@@ -23,44 +26,67 @@ $message="";
 
   <?php
 
-    if ($f->isSubmitted()) {
-      if ($f->isValid($params)) {
-        $logins = $f->getMembers();
-        $email=$_POST["email"];
-        $mdp=$_POST["password"];
-          if ($f->checkAcces($logins,$email,$mdp)) {
-            $f->connect();
-          }
-          else{
-            $message = "Email ou mot de passe incorrect";
-          }
-      }else{
-        $errors=$f->getErrors();
+  if ($f->isSubmitted()) {
+    if ($f->isValid($params)) {
+      $logins = $f->getMembers();
+      $email = $_POST["email"];
+      $mdp = $_POST["password"];
+      if ($f->checkAcces($logins, $email, $mdp)) {
+        $f->connect();
+      } else {
+        $message = "Email ou mot de passe incorrect";
       }
+    } else {
+      $errors = $f->getErrors();
     }
-
+  }
   ?>
-
-  <form action="#" method="POST">
+  <main>
     <div>
-      <label for="email">Email</label>
-      <input type="email" name="email" id="email" placeholder="votremail@exemple.com" value="<?php if($f->isSubmitted()){if(!empty($_POST["email"])){echo $_POST["email"];}}?>">
-      <p><?php if($f->isSubmitted()){if(!($f->isValid($params))){echo $errors["email"];}}?></p>
+      <div class="img-cont"><img src="./assets/images/logo.png" alt="logo de panza"></div>
+      <div class="content-cont">
+        <h1>Bienvenue chez <span>Panza</span>.</h1>
+        <p>La troupe de théâtre bordealaise est prête à <span>vous accueillir</span> et à partager notre <span>passion</span>.</p>
+      </div>
+      <div>
+        <img src="" alt="logo">
+        <p>Fait par la Kaizen Agency</p>
+      </div>
     </div>
-    <div>
-      <label for="password">Mot de passe</label>
-      <input type="password" name="password" id="password" placeholder="*************">
-      <p><?php if($f->isSubmitted()){if(!($f->isValid($params))){echo $errors["mdp"];}}?></p>
-    </div>
-    <div>
-      <input type="checkbox" name="checkbox" id="checkbox">
-      <label for="checkbox">Se souvenir de moi</label>
-    </div>
-    <div>
-      <a href="#">Mot de passe oublié ?</a>
-    </div>
-    <button>Se connecter</button>
-  </form>
+    <div></div>
+    <form action="#" method="POST">
+      <div>
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email" placeholder="votremail@exemple.com" value="<?php if ($f->isSubmitted()) {
+                                                                                                  if (!empty($_POST["email"])) {
+                                                                                                    echo $_POST["email"];
+                                                                                                  }
+                                                                                                } ?>">
+        <p><?php if ($f->isSubmitted()) {
+              if (!($f->isValid($params))) {
+                echo $errors["email"];
+              }
+            } ?></p>
+      </div>
+      <div>
+        <label for="password">Mot de passe</label>
+        <input type="password" name="password" id="password" placeholder="*************">
+        <p><?php if ($f->isSubmitted()) {
+              if (!($f->isValid($params))) {
+                echo $errors["mdp"];
+              }
+            } ?></p>
+      </div>
+      <div>
+        <input type="checkbox" name="checkbox" id="checkbox">
+        <label for="checkbox">Se souvenir de moi</label>
+      </div>
+      <div>
+        <a href="#">Mot de passe oublié ?</a>
+      </div>
+      <button>Se connecter</button>
+    </form>
+  </main>
 
   <p><?= $message ?></p>
 
--- spectacle.php
+++ spectacle.php
@@ -1,1 +1,29 @@
 <!-- Pour creer un spectacle -->
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Création de spectacle</title>
+</head>
+
+<body>
+  <header>
+    <nav></nav>
+  </header>
+  <main>
+    <h1>Créer un spectacle</h1>
+    <form method="post">
+      <div><label for="name">Nom du spectacle</label><input type="text" id="name" name="name"></div>
+      <div><label for="category">Catégorie</label><input type="text" id="category" name="category"></div>
+      <div><label for="date">Date</label><input type="text" id="date" name="date"></div>
+      <div><label for="start_time">Heure de début</label><input type="text" id="start_time" name="start_time"></div>
+      <div><label for="address">Adresse</label><input type="text" id="address" name="address"></div>
+      <div><label for="nb_comedians">Nombre de comédiens</label><input type="text" id="nb_comedians" name="nb_comedians"></div>
+      <div><label for="description">Description</label><textarea id="description" name="description"></textarea></div>
+    </form>
+  </main>
+</body>
+
+</html>
