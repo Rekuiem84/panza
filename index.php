e241ec06fc3bed2f24055fc1d50cb1c0bfd5b30f
Rekuiem84 <morinijoshua@gmail.com>
2024-03-13 09:48:03 +0100

    style de index.php

--- index.php
+++ index.php
@@ -18,14 +18,14 @@ $message = "";
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Panza - Troupe de théatre</title>
+  <title>Panza - Troupe de théâtre</title>
   <link rel="stylesheet" href="./assets/style/style.css">
+  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
 
 <body>
 
   <?php
-
   if ($f->isSubmitted()) {
     if ($f->isValid($params)) {
       $logins = $f->getMembers();
@@ -41,54 +41,58 @@ $message = "";
     }
   }
   ?>
-  <main>
-    <div>
+  <main class="login">
+    <div class="login__title-cont">
       <div class="img-cont"><img src="./assets/images/logo.png" alt="logo de panza"></div>
       <div class="content-cont">
-        <h1>Bienvenue chez <span>Panza</span>.</h1>
-        <p>La troupe de théâtre bordealaise est prête à <span>vous accueillir</span> et à partager notre <span>passion</span>.</p>
+        <h1 class="title">Bienvenue chez <span class="color-gradient">Panza</span>.</h1>
+        <p>La troupe de théâtre bordelaise est prête à <span class="color-gradient">vous accueillir</span> et à vous partager notre <span class="color-gradient">passion</span>.</p>
       </div>
       <div>
-        <img src="" alt="logo">
-        <p>Fait par la Kaizen Agency</p>
+        <i class='bx bx-share-alt'></i>
+        <p>Fait par la <span>Kaizen Agency</span></p>
       </div>
     </div>
-    <div></div>
-    <form action="#" method="POST">
-      <div>
-        <label for="email">Email</label>
-        <input type="email" name="email" id="email" placeholder="votremail@exemple.com" value="<?php if ($f->isSubmitted()) {
-                                                                                                  if (!empty($_POST["email"])) {
-                                                                                                    echo $_POST["email"];
-                                                                                                  }
-                                                                                                } ?>">
-        <p><?php if ($f->isSubmitted()) {
-              if (!($f->isValid($params))) {
-                echo $errors["email"];
-              }
-            } ?></p>
-      </div>
-      <div>
-        <label for="password">Mot de passe</label>
-        <input type="password" name="password" id="password" placeholder="*************">
-        <p><?php if ($f->isSubmitted()) {
-              if (!($f->isValid($params))) {
-                echo $errors["mdp"];
-              }
-            } ?></p>
-      </div>
-      <div>
-        <input type="checkbox" name="checkbox" id="checkbox">
-        <label for="checkbox">Se souvenir de moi</label>
-      </div>
-      <div>
-        <a href="#">Mot de passe oublié ?</a>
-      </div>
-      <button>Se connecter</button>
-    </form>
+    <div class="login__form-cont">
+      <div class="img-cont"><img src="./assets/images/logo-vertical-noir.png" alt="logo de panza"></div>
+      <h2 class="underline">Se connecter</h2>
+      <form class="login__form" method="POST">
+        <div class="login__field">
+          <label for="email">Email</label>
+          <input type="email" name="email" id="email" placeholder="votremail@exemple.com" value="
+          <?php if ($f->isSubmitted()) {
+            if (!empty($_POST["email"])) {
+              echo $_POST["email"];
+            }
+          } ?>">
+          <p><?php if ($f->isSubmitted()) {
+                if (!($f->isValid($params))) {
+                  echo $errors["email"];
+                }
+              } ?></p>
+        </div>
+        <div class="login__field">
+          <label for="password">Mot de passe</label>
+          <input type="password" name="password" id="password" placeholder="••••••••••">
+          <p><?php if ($f->isSubmitted()) {
+                if (!($f->isValid($params))) {
+                  echo $errors["mdp"];
+                }
+              } ?></p>
+        </div>
+        <div class="login__field--additional">
+          <div>
+            <input type="checkbox" name="checkbox" id="checkbox">
+            <label for="checkbox">Se souvenir de moi</label>
+          </div>
+          <a href="#">Mot de passe oublié ?</a>
+        </div>
+        <button>Se connecter</button>
+      </form>
+      <p><?= $message ?></p>
+    </div>
   </main>
 
-  <p><?= $message ?></p>
 
 </body>
 
